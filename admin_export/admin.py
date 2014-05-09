b1656cf6284bc7fcc2ea9b7c027d92734c8b6eb1
Vincent Alsteen <vincent.alsteen@bics.com>
2014-05-09 12:41:24 +0200

    Integration issues - use named urls to easily handle site prefix

--- admin_export/admin.py
+++ admin_export/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 
 def export_simple_selected_objects(modeladmin, request, queryset):
@@ -10,8 +11,8 @@ def export_simple_selected_objects(modeladmin, request, queryset):
     ct = ContentType.objects.get_for_model(queryset.model)
     if len(selected) > 10000:
         request.session['selected_ids'] = selected
-        return HttpResponseRedirect("/admin_export/export_to_xls/?ct=%s&ids=IN_SESSION" % (ct.pk,))
+        return HttpResponseRedirect(reverse("export_to_xls") + "?ct=%s&ids=IN_SESSION" % (ct.pk,))
     else:
-        return HttpResponseRedirect("/admin_export/export_to_xls/?ct=%s&ids=%s" % (ct.pk, ",".join(selected)))
+        return HttpResponseRedirect(reverse("export_to_xls") + "?ct=%s&ids=%s" % (ct.pk, ",".join(selected)))
 export_simple_selected_objects.short_description = "Export selected items to XLS"
 admin.site.add_action(export_simple_selected_objects)
